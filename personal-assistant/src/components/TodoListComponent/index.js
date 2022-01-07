93a469556b9e0c73439973ba0b7156eaddf8789d
Andrew Holmes <acholmes001@gmail.com>
2022-01-07 16:16:23 +0000

    Reactive list elements working using State

--- personal-assistant/src/components/TodoListComponent/index.js
+++ personal-assistant/src/components/TodoListComponent/index.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { TodoList, AddTodo, TodoH2, TodoItem } from './styles'
+import TodoReactiveComponent from '../TodoReactiveComponent'
 
 const TodoListComponent = () => {
     return(
@@ -8,6 +9,7 @@ const TodoListComponent = () => {
             <TodoH2>Your Todo List</TodoH2>
             <TodoItem>test text that needs to be really long to see if I can break stuff</TodoItem>
             <TodoItem>Test 2</TodoItem>
+            <TodoReactiveComponent/>
         </TodoList>
         <AddTodo>
             <h3>Add a Todo!</h3>
