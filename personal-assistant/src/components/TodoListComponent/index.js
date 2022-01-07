4fc5684094935b8783e01da7f26b3620915879f4
Andrew Holmes <acholmes001@gmail.com>
2022-01-07 21:19:30 +0000

    strikethrough functionality complete

--- personal-assistant/src/components/TodoListComponent/index.js
+++ personal-assistant/src/components/TodoListComponent/index.js
@@ -1,21 +1,22 @@
 import React, { useState } from 'react'
-import { TodoList, AddTodo, TodoH2, TodoItem } from './styles'
+import { TodoList, TodoH2, TodoItem } from './styles'
 import TodoReactiveComponent from '../TodoReactiveComponent'
+import AddTodoComponent from '../AddTodoComponent'
 
 const TodoListComponent = () => {
+    const [counter, updateCounter] = useState(0);
+    const [value, setValue] = useState('');
+    const [state, setState] = useState([]);
+
+    console.log({state})
+
     return(
     <>
         <TodoList>
             <TodoH2>Your Todo List</TodoH2>
-            <TodoItem>test text that needs to be really long to see if I can break stuff</TodoItem>
-            <TodoItem>Test 2</TodoItem>
-            <TodoReactiveComponent/>
+            <TodoReactiveComponent todos = {state} updateTodos = { setState }/>
         </TodoList>
-        <AddTodo>
-            <h3>Add a Todo!</h3>
-            <input type = "text" placeholder='Add Todo'></input>
-            <button>submit</button>
-        </AddTodo>
+        <AddTodoComponent todos = {setState} setTodos = {setState} counter = {counter} setCounter = {updateCounter} value = {value} setValue = {setValue} ></AddTodoComponent>
     </>)
 }
 
