537b19fd2e57e8f5575c8cd03dc96b4a685b181c
Andrew Holmes <acholmes001@gmail.com>
2022-01-14 17:39:36 +0000

    madlibs story done

--- 1_PM/madlibs.js
+++ 1_PM/madlibs.js
@@ -5,16 +5,22 @@ using string variables in the place of certain
 nouns, adjectives, and verbs */
 
 /* Variables, i.e.
-const noun1 = Gnorman Gnommington
-const verb1 = ______
 
 Change thses values to generate a different story
 */
 
+const noun1 = "Gnorman Gnommington"
+const pospronoun1 = "His"
+const adj2 = "frightful"
+const noun2 = "beast"
+const adv1 = "disgustingly"
+const verb1 = "trampled"
+const phrase1 = "change the local weather conditions temporarily to a most acidic rain"
+
 const story = `
 Here's a story about your favourite garden gnome ${noun1}. 
 ${pospronoun1} day had been quite unremarkable until he was suddenly interrupted from his highly valued and most crucial gnomial duties by a ${adj2} ${noun2},
-who quite ${adj3} ${verb1} His Gnomial Lordship and proceeded to ${phrase1}. How HGL (His Gnomial Lordship) ${noun1} will ever quite recover is yet to be seen,
+who quite ${adv1} ${verb1} His Gnomial Lordship and proceeded to ${phrase1}. How HGL (His Gnomial Lordship) ${noun1} will ever quite recover is yet to be seen,
 although few gnomes have been gnown to have such gnormous poise and resolve as HGL ${noun1}.
 `
 
